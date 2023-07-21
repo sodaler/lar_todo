859d5a7dd292633acdedb5c6db7b308358fa6710
sodaler <misha_lector87@mail.ru>
2023-07-21 19:27:10 +0400

    Commands

---
+++ app/Console/Commands/InstallCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'todo:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Base installation';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('migrate');
+        $this->call('queue:listen');
+
+        return self::SUCCESS;
+    }
+}
